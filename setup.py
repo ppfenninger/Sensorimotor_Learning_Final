c2d50103d5f73e0aefaf4aa8f9c27ecf5544faac
ppfenninger <ppfenni@gmail.com>
2023-05-02 18:10:14 -0400

    made an edit

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='deepexploration',
-    packages=find_packages(
-            where='deepexploration',
-        ),
-    package_dir={"": "deepexploration"},
+    packages=['deepexploration'],
     version='0.1.1',
     description='An deep exploration reinforcement learner',
     author='Me',
