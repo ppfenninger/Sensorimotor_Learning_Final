5f73e276853e85c01a2183e48030e4de8845955b
ppfenninger <ppfenni@gmail.com>
2023-05-02 11:57:11 -0400

    working on python packages

--- deepexploration/__init__.py
+++
@@ -1,3 +0,0 @@
-from de_agent import DeepExplorationAgent
-from de_engine import DeepExplorationEngine, TrajDataset, train_de_agent, eval_agent
-from de_runner import DeepExplorationRunner
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='deepexploration',
-    packages=find_packages(include=['deepexploration']),
+    packages=find_packages(),
     version='0.1.0',
     description='An deep exploration reinforcement learner',
     author='Me',
